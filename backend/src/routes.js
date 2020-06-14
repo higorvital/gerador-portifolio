491ced03534b20f8c87617305f57d7ece25b2c43
higorvital <h.vitall96@gmail.com>
2020-06-14 16:13:53 -0300

    rota dinamica

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,13 +2,11 @@ const {Router} = require('express');
 
 const UserController = require('./app/controllers/UserController');
 
-
 const routes = new Router();
 
-
-routes.get('/user/:slug', UserController.index);
-
 routes.post('/user', UserController.store);
+
+routes.get('/:slug', UserController.index);
 
 
 
